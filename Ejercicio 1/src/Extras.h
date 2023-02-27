52c9d122cf4b2884da68307d26cee7309c82d75a
Guido Tello <lpmaleinn@gmail.com>
2023-02-27 14:59:04 -0300

    Added Extras Header

---
+++ Ejercicio 1/src/Extras.h
@@ -0,0 +1,18 @@
+#ifndef EXTRAS_H
+#define EXTRAS_H
+
+namespace Final_Tello
+{
+
+	enum class Scene_Name
+	{
+		MENU,
+		GAMEPLAY,
+		GAMEOVER
+	};
+
+	static bool gameShouldClose = false;
+
+}
+
+#endif
